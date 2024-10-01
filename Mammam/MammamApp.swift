67a2b0d9ce0f8b40383543a0c06be16f6e062fdc
Michelle Angela Aryanto <aryanto.michelle@gmail.com>
2024-10-01 09:15:40 +0700

    Initial Commit

---
+++ Mammam/MammamApp.swift
@@ -0,0 +1,17 @@
+//
+//  MammamApp.swift
+//  Mammam
+//
+//  Created by Michelle Angela Aryanto on 01/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MammamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MammamTests/MammamTests.swift
@@ -0,0 +1,17 @@
+//
+//  MammamTests.swift
+//  MammamTests
+//
+//  Created by Michelle Angela Aryanto on 01/10/24.
+//
+
+import Testing
+@testable import Mammam
+
+struct MammamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
