0ce299b44360ced83a2247fb5a922196ff6f0052
Michelle Angela Aryanto <aryanto.michelle@gmail.com>
2024-10-02 15:56:45 +0700

    added firebase

--- Mammam/App/MammamApp.swift
+++ Mammam/App/MammamApp.swift
@@ -5,13 +5,28 @@
 //  Created by Michelle Angela Aryanto on 01/10/24.
 //
 
+import Firebase
 import SwiftUI
 
 @main
 struct MammamApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool
+    {
+        FirebaseApp.configure()
+        
+        return true
     }
 }
