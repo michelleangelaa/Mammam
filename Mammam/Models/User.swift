6a012ee84ab151ba86b10aec5019b2002bb5da62
Michelle Angela Aryanto <aryanto.michelle@gmail.com>
2024-10-04 16:06:21 +0700

    add user and .gitignore

---
+++ Mammam/Models/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  Mammam
+//
+//  Created by Michelle Angela Aryanto on 03/10/24.
+//
+
+import Foundation
+
+struct User: Identifiable, Codable {
+    let id: String
+    let fullname: String
+    let email: String
+    let username: String
+    var profileImageUrl: String?
+}
