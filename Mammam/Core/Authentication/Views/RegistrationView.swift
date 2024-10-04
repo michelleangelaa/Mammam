803fdd5f2f819137c20ee6cbefc20d8e349caa48
Michelle Angela Aryanto <aryanto.michelle@gmail.com>
2024-10-04 16:05:42 +0700

    Add .gitignore file

---
+++ Mammam/Core/Authentication/Views/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  Mammam
+//
+//  Created by Michelle Angela Aryanto on 03/10/24.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
