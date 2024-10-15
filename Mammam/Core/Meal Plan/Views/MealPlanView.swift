8c9860ed690ed86916f0e547d2d3e9825bde9d93
Michelle Angela Aryanto <aryanto.michelle@gmail.com>
2024-10-15 21:29:17 +0700

    added meal plan view

---
+++ Mammam/Core/Meal Plan/Views/MealPlanView.swift
@@ -0,0 +1,18 @@
+//
+//  MealPlanView.swift
+//  Mammam
+//
+//  Created by Michelle Angela Aryanto on 15/10/24.
+//
+
+import SwiftUI
+
+struct MealPlanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MealPlanView()
+}
