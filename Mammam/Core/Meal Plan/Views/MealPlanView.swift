9f339ba4705a1c6c1afdbed84b121126cf69ada5
Michelle Angela Aryanto <aryanto.michelle@gmail.com>
2024-10-18 00:12:00 +0700

    Explore: scrollable sheet

--- Mammam/Core/Meal Plan/Views/MealPlanView.swift
+++ Mammam/Core/Meal Plan/Views/MealPlanView.swift
@@ -9,28 +9,83 @@ import SwiftUI
 
 struct MealPlanView: View {
     @EnvironmentObject private var coordinator: Coordinator
-    @State private var navigateToRateMeal = false  // State to trigger navigation
+    @State private var navigateToRateMeal = false // State to trigger navigation
 
-    
-    
     var body: some View {
-        NavigationStack{
-            VStack {
-                Text("this is meal plan view")
-                    .kerning(0.2)
+        NavigationStack {
+            ScrollView {
+                VStack {
+                    Text("this is meal plan view")
+                        .kerning(0.2)
+
+                    Button {
+                        navigateToRateMeal = true
+                        //            coordinator.presentSheet(sheet: .rateMeal)
+                    } label: {
+                        Text("forgot password")
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .navigationDestination(isPresented: $navigateToRateMeal) {
+                        RateMealView() // This will show RateMealView within the same sheet
+                    }
+
+                    Text("Ingredients")
+                    VStack(alignment: .leading) {
+                        Text("A List Item")
+                        Text("A Second List Item")
+                        Text("A Third List Item")
+                        Text("A List Item")
+                        Text("A Second List Item")
+                        Text("A Third List Item")
+                        Text("A List Item")
+                        Text("A Second List Item")
+                        Text("A Third List Item")
+                        Text("A List Item")
+                        Text("A Second List Item")
+                        Text("A Third List Item")
+                        Text("A List Item")
+                        Text("A Second List Item")
+                        Text("A Third List Item")
+                        Text("A List Item")
+                        Text("A Second List Item")
+                        Text("A Third List Item")
+                        Text("A List Item")
+                        Text("A Second List Item")
+                        Text("A Third List Item")
+                        Text("A List Item")
+                        Text("A Second List Item")
+                        Text("A Third List Item")
+                        Text("A List Item")
+                        Text("A Second List Item")
+                        Text("A Third List Item")
+                        Text("A List Item")
+                        Text("A Second List Item")
+                        Text("A Third List Item")
+                        Text("A List Item")
+                        Text("A Second List Item")
+                        Text("A Third List Item")
+                        Text("A List Item")
+                        Text("A Second List Item")
+                        Text("A Third List Item")
+                        Text("A List Item")
+                        Text("A Second List Item")
+                        Text("A Third List Item")
+                        Text("A List Item")
+                        Text("A Second List Item")
+                        Text("A Third List Item")
+                        Text("A List Item")
+                        Text("A Second List Item")
+                        Text("A Third List Item")
+                        Text("A List Item")
+                        Text("A Second List Item")
+                        Text("A Third List Item")
+                        Text("A List Item")
+                        Text("A Second List Item")
+                        Text("A Third List Item")
+                    }
+                }
             }
-            Button {
-                navigateToRateMeal = true
-    //            coordinator.presentSheet(sheet: .rateMeal)
-            } label: {
-                Text("forgot password")
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .navigationDestination(isPresented: $navigateToRateMeal) {
-                            RateMealView()  // This will show RateMealView within the same sheet
-                        }
         }
-        
     }
 }
 
