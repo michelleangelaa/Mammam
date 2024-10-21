3b931cb6ec631be2259b2ccb98fc445a0a353b5d
Michelle Angela Aryanto <aryanto.michelle@gmail.com>
2024-10-21 23:06:37 +0700

    feat: added pre-defined pages on Notion

---
+++ Mammam/Core/Meal Plan/Views/GenerateMealView.swift
@@ -0,0 +1,18 @@
+//
+//  GenerateMealView.swift
+//  Mammam
+//
+//  Created by Michelle Angela Aryanto on 21/10/24.
+//
+
+import SwiftUI
+
+struct GenerateMealView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GenerateMealView()
+}
