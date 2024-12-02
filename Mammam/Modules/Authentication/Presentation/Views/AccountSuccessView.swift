cb7e7754b283ae46cfe63ecce45574bd5c320fa2
Michelle Angela Aryanto <aryanto.michelle@gmail.com>
2024-12-03 00:36:32 +0700

    Created several views

---
+++ Mammam/Modules/Authentication/Presentation/Views/AccountSuccessView.swift
@@ -0,0 +1,18 @@
+//
+//  AccountSuccessView.swift
+//  Mammam
+//
+//  Created by Michelle Angela Aryanto on 02/12/24.
+//
+
+import SwiftUI
+
+struct AccountSuccessView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccountSuccessView()
+}
