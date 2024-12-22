a5c41f0e289e5a8c142f35203f376e170f565427
Michelle Angela Aryanto <aryanto.michelle@gmail.com>
2024-12-22 16:50:56 +0700

    Ingredient and Nutrient are related

--- Mammam/Modules/Meal Plan/Data/Samples/NutritionSamples.swift
+++ Mammam/Modules/Meal Plan/Data/Samples/NutritionSamples.swift
@@ -8,14 +8,14 @@
 import Foundation
 
 extension Nutrient {
-    static var sampleNutritions : [Nutrient] {
+    static var sampleNutrients: [Nutrient] {
         [
             Nutrient(name: "Protein", nutrientCount: 0),
             Nutrient(name: "Fat", nutrientCount: 0),
             Nutrient(name: "Zinc", nutrientCount: 0),
             Nutrient(name: "MicroNutrient", nutrientCount: 0),
             Nutrient(name: "Iron", nutrientCount: 0),
-            Nutrient(name: "Carbo", nutrientCount: 0)
+            Nutrient(name: "Carbo", nutrientCount: 0),
         ]
     }
 }
--- Mammam/Modules/Meal Plan/Views/MealDetailView.swift
+++ Mammam/Modules/Meal Plan/Views/MealDetailView.swift
@@ -8,13 +8,46 @@
 import SwiftUI
 
 struct MealDetailView: View {
+    let ingredient: Ingredient
+    
     var body: some View {
+        VStack(spacing: 16) {
+            Text(ingredient.name)
+                .font(.title)
+                .fontWeight(.semibold)
+            
+            if let ingredientImage = ingredient.image {
+                Image(ingredientImage)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 120, height: 120)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+            } else {
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(Color.gray.gray100)
+                    .frame(minWidth: 120, minHeight: 120)
+            }
+            
+            Text("Nutrients:")
+                .font(.headline)
+            
+            if let nutrients = ingredient.nutrients {
+                ForEach(nutrients, id: \.self) { nutrient in
+                    Text(nutrient.name)
+                }
+            } else {
+                Text("No nutrients available.")
+            }
+            
+            Spacer()
+        }
+        .padding()
         Text("this is meal detail view")
-        
-        
     }
 }
 
 #Preview {
-    MealDetailView()
+    NavigationStack {
+        MealDetailView(ingredient: Ingredient.sampleIngredients[1])
+    }
 }
