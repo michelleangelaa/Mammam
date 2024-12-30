0ba01e70860b3c2df9c02b94288dda222c284003
evelynsantoso24 <velyn.es24@gmail.com>
2024-12-30 11:28:22 +0700

    baby, user, motivation,article table

---
+++ Mammam/Modules/Authentication/Presentation/Views/Data/Baby.swift
@@ -0,0 +1,8 @@
+//
+//  Baby.swift
+//  Mammam
+//
+//  Created by Evelyn Santoso on 30/12/24.
+//
+
+import Foundation
---
+++ Mammam/Modules/Authentication/Presentation/Views/Data/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  Mammam
+//
+//  Created by Evelyn Santoso on 30/12/24.
+//
+
+import Foundation
